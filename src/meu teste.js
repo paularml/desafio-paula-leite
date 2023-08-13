2afdd447c946115fea842eeec2fec718f5e8e6f0
paularml <paula.rml@hotmail.com>
2023-08-13 17:08:59 -0300

    Deixando em branco os arquivos de teste

--- src/meu teste.js
+++
@@ -1,6 +0,0 @@
-const produtosPrincipais = ["cafe", "suco", "sanduiche", "salgado"]
-const listaDeProdutosArray = ["cafe", "suco"]
-const veririficaProdutoPrincipal = (elementoAtual) => produtosPrincipais.includes(elementoAtual)
-const temProdutoPrincipal = listaDeProdutosArray.some(veririficaProdutoPrincipal)
-
-console.log(temProdutoPrincipal)
